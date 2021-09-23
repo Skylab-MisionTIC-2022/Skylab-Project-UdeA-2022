74abd56b6bf953909cd315c0af28a6792617db8a
JuanVaquiro <jvaquiro23@itfip.edu.co>
2021-09-23 10:31:18 -0500

    integracion de archvios react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './css/styles.css';
+
+function App() {
+  return (
+    // Inicio del portal Colocar los respectivas estiquetas desde el <header>
+    // debajodo de <div className="App">
+    <div className="App">
+
+
+    <p >Hola este sera la Pag. Bievenidos </p>
+    <a href="https://example-login.com">Pag. Login</a>
+       
+      
+
+    </div>
+  );  
+}
+
+export default App;
