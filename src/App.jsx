42ec0139a2052fce3e1148917c478b8e5c3c2c5a
JuanVaquiro <jvaquiro23@itfip.edu.co>
2021-09-30 16:31:20 -0500

    configuracion home y enrutaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import 'css/styles.css';
 import Index from './pages/Index';
+import Home from 'pages/Home'
+import User from 'pages/User'
+import Products from 'pages/Products';
+import Sales from 'pages/Sales';
+import PrivateLayout from 'layouts/PrivateLayout';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-
-    <div className="App">
-
-       <Index />
-  
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/home">
+          <PrivateLayout>
+            <Home />
+          </PrivateLayout>
+          </Route>
+        <Route path="/">
+          <Index />
+        </Route>
+      </Switch>
+    </Router>
   );  
 }
 
