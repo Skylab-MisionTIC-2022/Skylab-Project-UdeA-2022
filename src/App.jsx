a9ad24467ae109b73424d60fbe25da1fb075f958
JuanVaquiro <jvaquiro23@itfip.edu.co>
2021-09-30 17:16:50 -0500

    arreglo de conflintos

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,4 @@
 import 'css/styles.css';
-<<<<<<< HEAD
-import Index from './pages/Index.jsx';
-import Usuarios from './pages/Usuarios';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import FormularioUsuarioEditar from './pages/FormularioUsuarioEditar';
-import FormularioUsuario from './pages/FormularioUsuario';
-
-
-function App() {
-  return (
-
-    <div>
-
-<Router>
-        <Switch>
-
-          
-          <Route path="/usuarios/editar" exact>
-            <FormularioUsuarioEditar />
-          </Route> 
-          <Route path="/usuarios/crear" exact>
-            <FormularioUsuario/>
-          </Route>  
-          <Route path="/usuarios" exact>
-            <Usuarios />
-          </Route>
-         
-
-          <Route path="/">
-            <Index />
-          </Route>
-
-
-        </Switch>
-      </Router>
-    </div>
-=======
 import Index from './pages/Index';
 import Home from 'pages/Home'
 import User from 'pages/User'
@@ -59,7 +22,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
->>>>>>> 2.home-layout
   );  
 }
 
